377292be3a3592a118f04be580f525ca356ac70a
Ryan Berckmans <ryanberckmans@gmail.com>
2019-01-31 21:43:57 -0600

    Update TODOs (one of them is to move them to github issues)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 // TODO:
-//   add a button to bottom of page to toggle hide/show of previously seen entries;
-//   localStorage max is 10MB per origin, so may want to timestamp/ttl entries or clear them out every so often.
+//   add buttons to top/bottom of page to toggle hide/show of previously seen entries
+//   add a button to bottom of page to "mark all as seen" without having to click next
+//   localStorage max is 10MB per origin, so may want to timestamp/ttl entries or clear them out every so often. Current proposal: lifo, store last seen timestamp with key, merge keys/timestamps to keep latest timestamp, evict oldest keys when saving if number of keys exceeds hardcoded amount, like maybe 3000 keys.
+//   add preferences to allow user to hide/show the already-seen UI elements
+//   actually support HN (just reddit now); hacker news has no root DOM node for each entry, instead a duo/trio of elements are used for each link. Have to update SocialEntry to support a list of HTMLElements to hide, and after that add support for HN.
+//   Move these TODOs into github issues
 
 import jQueryGlobal from "jquery";
 import Reddit from "./Reddit";
